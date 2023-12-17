f162721c4c8abddbba769513ac042bc1d79138b1
Chris Daly <cdaly@urbn.com>
2023-12-17 16:48:40 -0500

    refactor: move term into separate component

---
+++ src/lib/terminal.svelte
@@ -0,0 +1,15 @@
+<div class="container mx-auto w-5/12 p-4">
+	<div class="bg-taskBar flex w-auto self-center rounded-t-xl">
+		<div class="mx-1.5 ml-4 my-4 w-3 h-3 rounded-full bg-exit hover:bg-exitHover"></div>
+		<div class="mx-1.5 my-4 w-3 h-3 rounded-full bg-minimize"></div>
+		<div class="mx-1.5 my-4 w-3 h-3 rounded-full bg-expand"></div>
+	</div>
+	<div
+		class="bg-gradient-to-b from-slate-600 to-zinc-200 flex flex-col justify-center rounded-b-xl"
+	>
+		<p class="text-6xl text-center">Hello.</p>
+
+		<br />
+		<p class="text-6xl text-center my-8 mx-10">My name is Chris, and I'm a software developer.</p>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,7 @@
 <script lang="ts">
-	import Typewriter from 'svelte-typewriter';
-	let isHelloDone = false;
-	let isTextShowing = false;
-	let cursorColor = '#27272a';
-
-	function handleDone(detail: string) {
-		isHelloDone = true;
-	}
-
-	function showSocials(detail: string) {
-		isTextShowing = true;
-	}
+	import Term from '$lib/terminal.svelte';
 </script>
 
-<div
-	class="bg-gradient-to-b from-slate-600 to-zinc-200 flex flex-col justify-center"
-	style="--cursor-color:{cursorColor}"
->
-	<Typewriter interval="68" delay="420" on:done={handleDone}>
-		<p class="text-6xl text-center font-bold">Hello.</p>
-	</Typewriter>
-
-	{#if isHelloDone}
-		<Typewriter mode="loopOnce" interval="68" delay="675" on:done={showSocials}>
-			<br />
-			<p class="text-6xl text-center font-bold my-8 mx-10">
-				My name is Chris, and I'm a software developer.
-			</p>
-		</Typewriter>
-	{/if}
+<div class="flex h-screen flex-col items-center justify-center bg-neutral-900">
+	<Term />
 </div>
-<div></div>
